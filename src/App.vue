5b6ef8d75d905be1b60d7b57a9cb7721b15fdf7c
Maiken Madsen <maikenmadsen@Maikens-iMac.local>
2020-03-07 13:59:08 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="flex content-center flex-wrap h-200">
+      <img class="object-contain h-48 w-full " alt="Vue logo" src="./assets/logo.png"> 
+      <img class="object-contain h-40 w-full" alt="Vue logo" src="./assets/tailwind.png"> 
+    </div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
